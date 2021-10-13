62c98fedbb5505924005511e511a244038da08db
Max Lockwood <mlockwood@hotmail.co.uk>
2021-10-13 12:23:26 +0100

    Add styling to NotFound component

--- src/NotFound.js
+++ src/NotFound.js
@@ -3,11 +3,17 @@ import { Link } from "react-router-dom";
 const NotFound = () => {
   return (
     <div className="not-found">
-      <h2>Sorry</h2>
-      <p>That page cannot be found</p>
-      <Link to="/">Back to the homepage...</Link>
+      <h1>404</h1>
+      <h2>PAGE NOT FOUND!</h2>
+      <p>
+        Sorry, the page you are looking for might have been removed had it's
+        name changed or is temporarily unavailable.
+      </p>
+      <button>
+        <Link to="/">GO TO HOMEPAGE</Link>
+      </button>
     </div>
   );
-}
- 
+};
+
 export default NotFound;
